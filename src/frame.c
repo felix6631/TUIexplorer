3fe03ffe98b3f0c70a39ded77b37fb6d0d7c9b2b
felix6631 <second6630@gmail.com>
2026-04-13 14:27:36 +0900

    after midterm exam....

---
+++ src/frame.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <ncurses.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+int main() {
+    struct termios terminal;
+    tcgetattr(STDIN_FILENO,&terminal);
+    terminal.c_lflag &= ~(ECHO|ISIG|IEXTEN)
+}
